366b5e305210d9e1ee75b687781684a962639e6c
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-08-19 21:07:51 +0700

    Initial commit

---
+++ app/src/main/java/com/irvanw/chapter2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.irvanw.chapter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun FUNNY(){
+        var ANGKA: Int
+    }
+}
